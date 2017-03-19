7ee38e410013bbe8b432af07beaa5c4e24fd2d5d
misha-marinenko <misha.marinenko.offical@gmail.com>
2017-03-19 12:38:57 +0200

    A js link example added! Fixed bugs.
    Fixed htmlbodystart();
    and added htmllinkcss();

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 #Import the library:
 include "htmlkitphp.php";
 
-htmlstart($html = '');
-htmlheadstart($htmlhead = '');
-htmllinkcss($htmllinkcss = '');
-htmlother($htmlother= '<pre>htmlother(); example</pre>');
-htmltitle($htmltitle = 'your title');
-htmlheadend();
-htmlbodystart($html = '');
-htmlfromfile($htmlfromfile = 'http://example.com');
+htmlstart($html = ''); # a  simle <html> tag with addons for example htmlstart($html = 'encoding="utf-8"');
+htmlheadstart($htmlhead = '');# a  simle <head> tag with addons for example htmlstart($htmlhead = 'bgcolor="black"');
+htmllinkcss($htmllinkcss = '');# a  simle <style> tag with addons for example htmllinkcss($htmllinkcss = 'http://exmaple.com/exmaple.css');
+htmllinkjs($htmllinkjs = 'https://code.getmdl.io/1.3.0/material.min.js');# A simle <script src="..."> 
+htmlother($htmlother= '<pre>htmlother(); example</pre>');#  other tag
+htmltitle($htmltitle = 'your title');# a html <title> tag
+htmlheadend();#tag </head>
+htmlbodystart($htmlbody = '');# a simple <body>, exmaple: htmlbodystart($htmlbody = 'bgcolor="white"');
+htmlfromfile($htmlfromfile = 'http://example.com');# get html from URL or file
 htmlbodyend();
 htmlend();
 
